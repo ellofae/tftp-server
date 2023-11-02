54887cd9f923ef9b7ac052e8733caed6fc66211c
ellofae <sergei.bykovskiy2003@gmail.com>
2023-11-02 14:06:26 +0300

    feat: data packet is added

---
+++ packets/data_packet.go
@@ -0,0 +1,70 @@
+package packets
+
+import (
+	"bytes"
+	"encoding"
+	"encoding/binary"
+	"errors"
+	"io"
+)
+
+// DATA packet: Opeartion Code | Block Number | Payload
+// DATA packet's size: 2 bytes + 2 bytes + n bytes + 1
+
+type DataPacket struct {
+	BlockNumber uint16
+	Payload     io.Reader
+
+	encoding.BinaryMarshaler
+	encoding.BinaryUnmarshaler
+}
+
+func (dp *DataPacket) MarshalBinary() ([]byte, error) {
+	buf := new(bytes.Buffer)
+	buf.Grow(PacketSize)
+
+	dp.BlockNumber++
+
+	err := binary.Write(buf, binary.BigEndian, OperationDataResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	err = binary.Write(buf, binary.BigEndian, dp.BlockNumber)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = io.CopyN(buf, dp.Payload, DataBlockSize)
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func (dp *DataPacket) UnmarshalBinary(data []byte) error {
+	if l := len(data); l < 4 || l > PacketSize {
+		return errors.New("invalid DATA packet, length of a packet is incorrect")
+	}
+
+	buf := bytes.NewBuffer(data)
+
+	var code OperationCode
+	err := binary.Read(buf, binary.BigEndian, &code)
+	if err != nil || code != OperationDataResponse {
+		return errors.New("invalid DATA packet, packet operation code is incorrect")
+	}
+
+	err = binary.Read(buf, binary.BigEndian, &dp.BlockNumber)
+	if err != nil {
+		return errors.New("invalid DATA packet, unable to read packet's number")
+	}
+
+	err = binary.Read(buf, binary.BigEndian, dp.Payload)
+	if err != nil {
+		return errors.New("invalid DATA packet, unable to read the payload")
+	}
+
+	return nil
+}
--- packets/types.go
+++ packets/types.go
@@ -3,18 +3,18 @@ package packets
 // Maximum TFTP package size to avoid fragmention - 516 bytes
 // TFTP header's size - 4 bytes (Operation code and Block number)
 const (
-	PackageSize   = 516
-	DataBlockSize = PackageSize - 4
+	PacketSize    = 516
+	DataBlockSize = PacketSize - 4
 )
 
 type OperationCode uint8
 
 const (
-	OperationReadRequest OperationCode = iota + 1 // RRQ packet code
-	_                                             // WRQ packet code
-	OperatonDataResponse                          // DATA packet code
-	OperationAcknowledge                          // ACK packet code
-	OperationError                                // ERROR packet code
+	OperationReadRequest  OperationCode = iota + 1 // RRQ packet code
+	_                                              // WRQ packet code
+	OperationDataResponse                          // DATA packet code
+	OperationAcknowledge                           // ACK packet code
+	OperationError                                 // ERROR packet code
 )
 
 type ErrorCode uint8
